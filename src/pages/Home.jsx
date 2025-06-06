7e21d4f80b05476574a49e4a2ba2db8a27fd8da3
simoz <simon.ando92@gmail.com>
2025-06-07 08:47:45 +1000

    removed svg icons (phone and location) home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, EffectFade, Pagination, Navigation } from 'swiper/modules';
+import { Phone, MapPin } from 'lucide-react';
+
+
 
 // Import Swiper styles
 import 'swiper/css';
@@ -99,9 +102,7 @@ const Home = () => {
 
           <div className="flex justify-center items-center space-x-4 sm:space-x-6 mb-8 flex-wrap">
             <div className="flex items-center mb-2 sm:mb-0">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-6 sm:w-6 text-white mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-              </svg>
+              <Phone className="h-5 w-5 sm:h-6 sm:w-6 text-white mr-2" />
               <a 
                 href="tel:0475455654" 
                 className="text-sm sm:text-base text-white"
@@ -109,11 +110,9 @@ const Home = () => {
                 0475 455 654
               </a>
             </div>
+
             <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-6 sm:w-6 text-white mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 616 0z" />
-              </svg>
+              <MapPin className="h-5 w-5 sm:h-6 sm:w-6 text-white mr-2" />
               <span className="text-sm sm:text-base text-white">Mountain Creek</span>
             </div>
           </div>
