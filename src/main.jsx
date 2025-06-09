ea04d988e266f38a4f485d20f4932a892cedf9b5
simoz <simon.ando92@gmail.com>
2025-06-09 22:58:33 +1000

    added the vercel speed trackign properly

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 
 import './styles/theme.css';
 import './styles/index.css';
 
 injectSpeedInsights();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
