9612d8a15669aa7bbe1ef79a0bd09ec81607a7c8
simoz <simon.ando92@gmail.com>
2025-06-09 22:50:59 +1000

    add vercel speed tracking

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import './styles/theme.css';
 import './styles/index.css';
+
+injectSpeedInsights();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
